fa48a2b979614ba258ce66ad11fe0052e04d79b5
Nomi Vos <nvoswork@gmail.com>
2017-10-14 06:33:23 -0500

    initialize cucumber

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'coveralls'
+
+Coveralls.wear!
 
 ActiveRecord::Migration.maintain_test_schema!
 
